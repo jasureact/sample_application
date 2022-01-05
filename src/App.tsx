f12ff0512a09a94c79be3618485f62ce7e53d9cc
jasureact <jasurbekmardanov2000@gmail.com>
2022-01-05 23:41:09 +0500

    initialized application

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+    return (
+        <div className="wrapper">
+            <h1 className="main">React 17 and TypeScript 4 App!🚀</h1>
+        </div>
+    );
+};
+export default App;
